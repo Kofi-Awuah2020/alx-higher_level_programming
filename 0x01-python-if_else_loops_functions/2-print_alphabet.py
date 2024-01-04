10ea6bd3e155fcd7be6e3be91042c4b8c518adda
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-04 05:27:01 +0000

    Printing the alphabets in lowercase + I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-import string
-
-str = string.ascii_lowercase
-print(str)
+for char in range(97, 123):
+    print("{0:c}".format(char), end='')  # {:c} converts char to ascii alphabet
