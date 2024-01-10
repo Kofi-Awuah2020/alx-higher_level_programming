707abd939bb69b11a0376bc5e90c6e2f7cbab666
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-10 13:22:25 +0000

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for char in str:
         ascii_value = ord(char)
         if ascii_value >= 97 and ascii_value <= 122:
-            print('{0:c}'.format(ascii_value - 32), end='')
+            print("{0:c}".format(ascii_value - 32), end='')
         else:
-            print(char, end='')
+            print("{0:s}".format(char), end='')
     print()
