7c360440cc9de45e78bd6b735c5db17abff41ffb
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-10 21:16:08 +0000

    Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for columns in row:
-            if columns is not row[len(row) - 1]: #if columns is last element 
+            if columns is not row[len(row) - 1]:  # if columns is last element
                 print("{:d}".format(columns), end=" ")
             else:
                 print("{:d}".format(columns), end="")
