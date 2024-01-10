110ae10cb5149baa413c2b0790cfbbd2d5b94fc9
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-10 13:17:20 +0000

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for first_number in range(0, 9):
     for second_number in range(first_number + 1, 10):
-        if first_number + second_number != 17: 
+        if first_number + second_number != 17:
             print("{}{},".format(first_number, second_number), end=' ')
         else:
             print("{}{}".format(first_number, second_number))
