8d86c005ed714878a4d8a9acfe30b3ad9050c835
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-01-10 12:40:15 +0000

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    new_list = my_list[:]
+    for idx, number in enumerate(new_list):
+        if number % 2 == 0:
+            new_list[idx] = True
+        else:
+            new_list[idx] = False
+    return new_list
