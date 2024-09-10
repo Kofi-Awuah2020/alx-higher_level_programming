993bc379415854ba472637b0f900a727f0a5590a
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-09-10 20:15:41 +0000

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_number = ((number * -1) % 10) * -1
+    last_number = ((number * -1) % 10)
+    print(last_number)
 else:
     last_number = number % 10
 
