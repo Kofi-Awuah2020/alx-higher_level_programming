0479ca0aa6c2381d1703b9b7cd3ec62fd40da87c
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-09-11 21:50:14 +0000

    Remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -5,4 +5,5 @@ def remove_char_at(str, n):
     else:
         newstring = list(str)
         del newstring[n]
-        return ''.join(newstring) # Join methd concatenates the list into string
+        return ''.join(newstring)
+    # Join method concatenates the list into a string
