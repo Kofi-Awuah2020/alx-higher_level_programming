5b49d0569e343e780767aefd665f1f47fc8c623a
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-09-13 05:08:18 +0000

    Who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,9 +3,9 @@ import hidden_4
 
 
 def main():
-    names = (dir(hidden_4))
+    names = dir(hidden_4)
     filtered_names = [name for name in names if not name.starstwith('__')]
-    sorted_names = filtered_names.sort()
+    sorted_names = sorted(filtered_names)
 
     for name in sorted_names:
         print(name)
