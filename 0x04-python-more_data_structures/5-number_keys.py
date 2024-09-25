b6b789901e0d6c92fa45dc3acbccdf98517732e1
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-09-25 08:04:53 +0000

    Number of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    name = []
-    for key in a_dictionary.keys():
-        name.append(key)
-
-    return (len(name))
+    return (len(a_dictionary.keys()))
