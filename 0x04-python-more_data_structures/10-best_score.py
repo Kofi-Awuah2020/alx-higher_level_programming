4a74c82d8443ea05e84ac71ac57ee295d5f0b3e6
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-10-06 11:19:39 +0000

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary != None:
-        return max(tuple(a_dictionary.keys()))
-    else:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
+
+    max_value = 0
+    max_key = None
+
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+
+    return max_key
