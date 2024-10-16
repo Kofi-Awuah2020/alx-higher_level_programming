b0d985ddc37246414bce1c010b758cb6f72bf89f
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-10-16 22:23:29 +0000

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,11 +4,10 @@
 def read_file(filename=""):
     """
     Reads a file from a file
-    
+
     Args:
     filename(str): The name of the file to be read
     """
     with open(filename, mode="r", encoding="utf-8") as file:
         content = file.read()
         print(content)
-
