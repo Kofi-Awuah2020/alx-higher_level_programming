ba8c6c10730ddb1fe81216ad42e0e882eaff355e
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-10-16 20:49:08 +0000

    Safe Function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,6 @@ import sys
 def safe_function(fct, *args):
     try:
         fct(*args)
-    except Exception as e:
+    except BaseException as e:
         print("Exception:", e, file=sys.stderr)
         return None
