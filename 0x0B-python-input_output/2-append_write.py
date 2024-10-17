02ea7e7e310a4341f86862fe4623d47f72668bbb
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2024-10-17 05:02:34 +0000

    Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""A module with a function that appends to a file"""
+
+
+def append_write(filename="", text=""):
+    """Writes to a file
+
+    Args:
+    filename(str): The name of the file to be written to
+    text: text to be appended
+    """
+    with open(filename, mode="a", encode="utf-8") as file:
+        length = file.write(text)
+        return length
